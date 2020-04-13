98cf42bb03cce2297f6226cea9805ef8f5544103
Austin Moen <moenaustin20@gmail.com>
2020-04-13 10:47:52 -0500

    working program

---
+++ LinqProblems/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WordLibrary wordLibrary = new WordLibrary();
+            List<string> words = wordLibrary.GetWords();
+
+            var wordsWithTh = words.Where(w => w.Contains("th"));
+            var actualWord = wordsWithTh.Select(w => w).ToList();
+            foreach(var word in actualWord)
+            {
+                Console.WriteLine(word);
+            }
+
+        }
+    }
+}
---
+++ LinqProblems/WordLibrary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqProblems
+{
+    class WordLibrary
+    {
+        public List<string> GetWords()
+        {
+            return new List<string> { "the", "bike", "this", "it", "tenth", "mathematics" };
+        }
+
+        
+
+    }
+}
